488aa4da9e3fecaae6957e8466fff02bc937d3d3
Habil Yıldırım <135065802+YILDIRIMT@users.noreply.github.com>
2025-05-14 01:04:41 +0300

    Delete script directory

--- script/clear_list.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PARAMETER=$1
-cd data
-rm fav_data.csv
-touch fav_data.csv
--- script/find_port.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-current_ip=$1
-timeout 0.1 ping -c 1 $current_ip | sed -n '2p' | awk '{print $1}'
